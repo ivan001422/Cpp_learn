47752e8262aa8cfa2617f8fceef8d5839ef1e743
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-06 09:05:20 +0300

    Update Else_if.cpp

--- Cpp_list/Control_structures/Conditional_operators/Else_if.cpp
+++ Cpp_list/Control_structures/Conditional_operators/Else_if.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    
+    int a = 1, b = 2;
+
+    if (a > b) cout << "a > b"; // Условие не выполнится
+
+    else if (a % b == 1) cout << "a % b == 0" << "\n"; // Если ни 1 оператор перед else if не сработал, то else if проверит условие.
+
+    else cout << "a < b"; // Если ни 1 оператор перед else не сработал, то else сработает.
+}
 
