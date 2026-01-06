fc259a8d5b94dc80aa2d4bfb74a2934e01bf3f70
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-06 09:01:47 +0300

    Update Else.cpp

--- Cpp_list/Control_structures/Conditional_operators/Else.cpp
+++ Cpp_list/Control_structures/Conditional_operators/Else.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    /*
+    
+    Общий шаблон оператора else:
+
+    if (условие) инструкция или блок кода
+
+    else инструкция или блок кода
+    
+    */
+
+    if (1 > 2) cout << "1 > 2"; // Условие не выполнится
+
+    else cout << "1 < 2"; // Если ни 1 оператор перед else не сработал, то else сработает.
+}
 
