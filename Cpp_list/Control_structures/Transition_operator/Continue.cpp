f8fa68a41f3413cf2625c3500c8318f70e720587
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-07 05:52:46 +0300

    Update Continue.cpp

--- Cpp_list/Control_structures/Transition_operator/Continue.cpp
+++ Cpp_list/Control_structures/Transition_operator/Continue.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    for (int a = 0; a <= 20; a++) {
+        if (a % 2 == 0) { cout << a << "\n"; continue; } // Continue моментально переходит к следующей итерации цикла.
+        cout << "Нечетное\n";
+    }
+
+    // continue работает со всеми циклами
+}
