b2c4be00bb9720254344322d7ba1418b1b1a67b7
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-07 05:30:09 +0300

    Update Do_while.cpp

--- Cpp_list/Control_structures/Cycles/Do_while.cpp
+++ Cpp_list/Control_structures/Cycles/Do_while.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    short int a , b = 10;
+
+    do {
+        cout << "Введите число 10: "; // Сначала выполним инструкции, потом проверим условие для следующей итерации.
+        cin >> a;
+    } while (a != b); // Если условие = true, то цикл сработает еще раз.
+    
+    do cin >> a; // Можно использовать без блока кода
+    while (a != b);
+}
