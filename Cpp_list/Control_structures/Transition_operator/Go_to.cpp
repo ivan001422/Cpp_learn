0dcdf2127d030bedafecc2e000d2eab2a978cc68
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-07 06:55:26 +0300

    Update Go_to.cpp

--- Cpp_list/Control_structures/Transition_operator/Go_to.cpp
+++ Cpp_list/Control_structures/Transition_operator/Go_to.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    for (int x = 0; x < 1000; x++) {
+        if (x == 10) goto stop; // Переход на метку
+        else cout << x << "\n";
+    }
+    stop: // Идентификатор
+        cout << "\n";
+        cout << "stop";
+
+}
