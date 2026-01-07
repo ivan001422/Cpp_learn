a7d010ee004f3360af37e0f4f7025a0359fb9204
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-07 05:47:21 +0300

    Update Break.cpp

--- Cpp_list/Control_structures/Transition_operator/Break.cpp
+++ Cpp_list/Control_structures/Transition_operator/Break.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    int a = 0;
+
+    for (int x = 0; x <= 10; x++) {
+        if (x == 5) { break; } // Моментально завершает все итерации цикла
+        cout << x << "\n";
+    }
+
+    cout << "\n";
+
+    while (a != 10) { // Работает со всеми циклами
+        if (a == 5) { break; }
+        cout << a << "\n";
+        a++;
+    }
+
+    cout << "\n";
+
+    do {
+        if (a == 10) { break; }
+        cout << a << "\n";
+        a++;
+    } while (a != 20);
+}
