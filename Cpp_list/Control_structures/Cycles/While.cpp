6e8f1bbdafbd2933c9399104ce79e469aee645ac
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-07 05:24:39 +0300

    Update While.cpp

--- Cpp_list/Control_structures/Cycles/While.cpp
+++ Cpp_list/Control_structures/Cycles/While.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    short int a = 1, b = 10;
+
+    while (a <= b) { // Пока условие = true будут выполнятся инструкции
+        cout << a << "\n";
+        a++;
+    }  
+}
