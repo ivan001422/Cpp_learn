82326ba52bc4214a50076ef21e7bf0358c98e338
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-14 18:44:04 +0300

    Update project_5.cpp

--- Projects/project_5.cpp
+++ Projects/project_5.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <windows.h>
+#include <cmath>
 
+using namespace std;
+
+const int n = 5;
+
+int MIN(int *ip);
+
+int main() {
+	int arr[n] = { 7,2,3,4,5 };
+	int x = MIN(arr);
+	cout << x;
+}
+
+int MIN(int *ip) {
+	int* last = ip + n;
+	int min = pow(2 , 31) - 1;
+	for (ip; ip != last; ip++) {
+		if (*ip < min) min = *ip;
+	}
+	return min;
+}
