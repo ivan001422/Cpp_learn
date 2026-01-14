7e49d716d17792ae374b04d5a1bf09fff489192a
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-14 18:21:23 +0300

    Update project_4.cpp

--- Projects/project_4.cpp
+++ Projects/project_4.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
+
+int MIN(int a, int b);
+
+int main() {
+	int x = MIN(1, 2);
+	cout << x;
+}
+
+int MIN(int a, int b) {
+	if (a <= b) return a;
+	return b;
+}
