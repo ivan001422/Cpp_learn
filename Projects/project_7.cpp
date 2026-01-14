974268fb7190ccf1aa35be3cc350fbe503ceac27
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-14 20:41:12 +0300

    Update project_7.cpp

--- Projects/project_7.cpp
+++ Projects/project_7.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <windows.h>
+#include <cmath>
 
+using namespace std;
+
+double distance(double x, double y, double xc, double yc);
+
+bool IsPointInCircle(double x, double y, double xc, double yc, double r);
+
+int main() {
+	cout << IsPointInCircle(0.5, 0.5, 1, 1, 0.1);
+}
+
+double distance(double x, double y, double xc, double yc) {
+
+	double dx = pow(x - xc, 2);
+	double dy = pow(y - yc, 2);
+
+	double result = sqrt(dx + dy);
+
+	return result;
+}
+
+bool IsPointInCircle(double x, double y, double xc, double yc, double r) {
+	double point_distance = distance(x, y, xc, yc);
+
+	return point_distance < r;
+}
