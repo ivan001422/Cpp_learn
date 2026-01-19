78f68398b458d22db6ec3bc6dbc049aaa2da5721
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-19 16:49:35 +0300

    Update project_10.cpp

--- Projects/project_10.cpp
+++ Projects/project_10.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int SUM(int* arr, int* len);
+
+
+int main() {
+	
+	int z[] = { 1,2,3,4,5,6,7 };
+	cout << SUM(z , z + sizeof(z) / sizeof(int));
+}
+
+
+int SUM(int* arr, int* len) {
+	if (arr == len) return 0;
+	else return *arr + SUM(arr + 1, len);
+}
 
