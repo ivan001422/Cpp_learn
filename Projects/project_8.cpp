3c4c238ce2260635d8157aa016f3b977e2a8e835
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-01-19 14:36:44 +0300

    Update project_8.cpp

--- Projects/project_8.cpp
+++ Projects/project_8.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int factorial(int x);
+
+int main() {
+	cout << factorial(5);
+
+	return 0;
+}
+
+int factorial(int x) {
+	if (x == 1) return 1;
+	else return x * factorial(x - 1);
+}
